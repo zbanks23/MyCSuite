91338a3f40ea9c2c23f65bf62f683d98feaaf748
Zhicheng Li <Zli40547@gmail.com>
2025-12-17 13:45:43 -0500

    feat: Implement initial home screen UI using `ThemedView` and `ScreenHeader`.

--- apps/mycpo/app/(tabs)/index.tsx
+++ apps/mycpo/app/(tabs)/index.tsx
@@ -1,5 +1,10 @@
+import { ThemedView } from '../../components/ui/ThemedView';
+import { ScreenHeader } from '../../components/ui/ScreenHeader';
+
 export default function HomeScreen() {
   return (
-    <></>
+    <ThemedView className="flex-1 p-4">
+          <ScreenHeader title="Home" />
+    </ThemedView>
   );
 }
